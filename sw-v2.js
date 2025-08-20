1e7468328759ea829cf22fd04e62260d28c11acf
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2025-08-20 23:40:12 +0300

    Add files via upload

---
+++ sw-v2.js
@@ -0,0 +1,39 @@
+
+const CACHE = 'shangshi-v2-722189';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css?v=722189',
+  './app.js?v=722189',
+  './menu.json',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('.json')) {
+    e.respondWith(
+      fetch(e.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return resp;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+  }
+});
