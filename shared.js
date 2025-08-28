20f0517b17c3581da52c531a6115e7806343d0fe
RicardoNeto94 <49795011+RicardoNeto94@users.noreply.github.com>
2025-08-28 13:34:15 +0300

    Delete shared.js

--- shared.js
+++
@@ -1,4 +0,0 @@
-export const LEGEND = {
-  CE:"Celery", GL:"Gluten", CR:"Crustaceans", EG:"Eggs", FI:"Fish", MO:"Molluscs", Mi:"Milk",
-  MU:"Mustard", NU:"Nuts", SE:"Sesame", SO:"Soya", GA:"Garlic", ON:"Onion", MR:"Mushrooms"
-};
